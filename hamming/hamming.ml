79cec824ecf01b1abb6ef7fbec809b1b8adacc28
ghostiee <yujw@ivymobi.com>
2019-11-28 16:51:11 +0800

    update match pattern

--- hamming/hamming.ml
+++ hamming/hamming.ml
@@ -1,13 +1,10 @@
 type nucleotide = A | C | G | T
 
 let hamming_distance dna_a dna_b =
-  let (len_a, len_b) = (List.length dna_a, List.length dna_b) in
-    if len_a != len_b then
-        match (len_a, len_b) with
-        | (0, _) -> Error "left strand must not be empty"
-        | (_, 0) -> Error "right strand must not be empty"
-        | _ -> Error "left and right strands must be of equal length"
-    else
-      Ok (List.fold_left2
-        (fun acc a b -> if a = b then acc else acc + 1) 0 dna_a dna_b
-      )
+  match (List.length dna_a, List.length dna_b) with
+  | (len_a, len_b) when len_a = len_b -> Ok (List.fold_left2
+      (fun acc a b -> if a = b then acc else acc + 1) 0 dna_a dna_b
+    )
+  | (0, _) -> Error "left strand must not be empty"
+  | (_, 0) -> Error "right strand must not be empty"
+  | _ -> Error "left and right strands must be of equal length"
