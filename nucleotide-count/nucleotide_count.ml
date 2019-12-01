a73088e72fca325316606ed4f2f9c177c3a959ac
ghostie <y_jw@outlook.com>
2019-12-01 13:22:30 +0800

    update monad

--- nucleotide-count/nucleotide_count.ml
+++ nucleotide-count/nucleotide_count.ml
@@ -7,20 +7,19 @@ let is_valid_nucleotide = function
 | 'A' | 'C' | 'G' | 'T' -> Ok ()
 | x -> Error x
 
-let fold_nucleotide s ~f ~init =
+let fold_nucleotides s ~f ~init =
   String.fold s ~init:init ~f: (fun acc dna ->
     is_valid_nucleotide dna >>= fun _ ->
-    acc >>= fun a ->
-      Ok (f a dna)
+      acc >>| fun a -> f a dna
   )
 
 let count_nucleotide s c =
   is_valid_nucleotide c >>= fun _ ->
-  fold_nucleotide s ~init:(Ok 0) ~f:(fun acc dna ->
+  fold_nucleotides s ~init:(Ok 0) ~f:(fun acc dna ->
     if (Char.equal dna c) then acc + 1 else acc
   )
 
 let count_nucleotides s =
-  fold_nucleotide s ~init:(Ok empty) ~f:(fun acc dna ->
+  fold_nucleotides s ~init:(Ok empty) ~f:(fun acc dna ->
     Map.update acc dna ~f: (function None -> 1 | Some v -> v + 1)
   )
